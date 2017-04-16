50e7fb5be6ca8c42a29f8e597c2b7668ac9e2d35
danhumphrey216 <danhumphrey216@users.noreply.github.com>
2017-04-16 16:38:46 -0400

    Condensed Attrition Tables

--- Afghan - All Parts Combined.R
+++ Afghan - All Parts Combined.R
@@ -107,33 +107,25 @@ balancetable
 #summary stats
 sum_list <- list(afghan$test_observed,
                  afghan$test_observed[treatment == 1],
-                 afghan$test_observed[treatment == 0],
-                 afghan$test_score_normalized,
-                 afghan$test_score_normalized[treatment == 1],
-                 afghan$test_score_normalized[treatment == 0]
+                 afghan$test_observed[treatment == 0]
 )
 n <- sapply(sum_list, function(x) length(which(!is.na(x))))
 mean <- sapply(sum_list, mean, na.rm = T)
 sd <- sapply(sum_list, sd, na.rm = T)
-min <- sapply(sum_list, min, na.rm = T)
-median <- sapply(sum_list, median, na.rm = T)
-max <- sapply(sum_list, max, na.rm = T)
-sum_table<-cbind(n,mean,sd,min,median,max)
+sum_table<-cbind(n,mean,sd)
 sum_table<-round(sum_table,digits=3)
-rownames(sum_table) <- c('Test Taken','Test Taken for Treatment', 'Test Taken for Control',
-                         'Normalized Test Score', 'Test Score for Treatment', 'Test Score for Control')
+rownames(sum_table) <- c('% Test Taken - All','% Test Taken - Treatment', '% Test Taken - Control')
 sum_table
 
-#creates list of variable for passing to loops, skips ids
-varlist <- names(afghan)[c(2:20, 22:25)]
 #creating a dataset of only the attritted
 afghanattrition <- afghan[!complete.cases(afghan),]
 
 #########################################################
 #comparisons of treatment and control for attritted only#
 #########################################################
-#omits test_observed[13], treatment[14] and test_score[19]
-attrition_by_treatment <-lapply(varlist[c(1:12,15:18,20:23)], function(x) {
+#omits test_observed[13], treatment[14], clustercode[15], and test_score[19]
+varlist
+attrition_by_treatment <-lapply(varlist[c(1:12,16:18,20:22)], function(x) {
   t.test(as.formula(paste(x,"treatment",sep="~")), data = afghanattrition
          , alternative = "two.sided", mu = 0, paired = FALSE, var.equal = FALSE, conf.level = 0.95)
 })
@@ -141,73 +133,18 @@ attrition_by_treatment <-lapply(varlist[c(1:12,15:18,20:23)], function(x) {
 #create table
 attrition_table <- t(sapply(attrition_by_treatment, function(x) {
   c(x$data.name,
-    x$estimate,
-    ci.lower = x$conf.int[1],
-    ci.upper = x$conf.int[2],
-    p.value = x$p.value,
-    "adj.p.value" = p.adjust(x$p.value, method = "bonferroni", n = length(x)),
-    x$parameter
+    "mean_crtl" = unname(x$estimate[1]),
+    "mean_trt" = unname(x$estimate[2]),
+    "diff_means" =unname(x$estimate[2])-unname(x$estimate[1]),
+    "adj.p.value" = p.adjust(x$p.value, method = "bonferroni", n = length(x))
   )
 }))
 rownames(attrition_table) <- attrition_table[,1]
 attrition_table <- attrition_table[,-c(1)]
 attrition_table <- data.frame(attrition_table, stringsAsFactors=FALSE)
 attrition_table[] <- lapply(attrition_table, function(x) as.numeric(as.character(x)))
-attrition_table<-format(round(attrition_table,3),digits =1)
+rownames(attrition_table) <- varlist[c(1:12,16:18,20:22)]
 attrition_table
-
-############################################
-#attritors vs compliants for treatment only#
-############################################
-#omits test_observed[13] and treatment[14] and test_score[19]
-attrition_by_test_observed <- lapply(varlist[c(1:12,15:18,20:23)], function(x) {
-  t.test(as.formula(paste(x,"test_observed",sep="~")), data =subset(afghan,treatment==1)
-         ,  alternative = "two.sided", mu = 0, paired = FALSE, var.equal = FALSE, conf.level = 0.95)})
-#create table and adujst p values
-attrition_by_treatment_table <- t(sapply(attrition_by_test_observed, function(x) {
-  c(x$data.name,
-    x$estimate,
-    ci.lower = x$conf.int[1],
-    ci.upper = x$conf.int[2],
-    p.value = x$p.value,
-    "adj.p.value" = p.adjust(x$p.value, method = "bonferroni", n = length(x)),
-    x$parameter
-  )
-}))
-rownames(attrition_by_treatment_table) <- attrition_by_treatment_table[,1]
-attrition_by_treatment_table <- attrition_by_treatment_table[,-c(1)]
-attrition_by_treatment_table <- data.frame(attrition_by_treatment_table, stringsAsFactors=FALSE)
-attrition_by_treatment_table[] <- lapply(attrition_by_treatment_table, function(x) as.numeric(as.character(x)))
-attrition_by_treatment_table<-format(round(attrition_by_treatment_table,3),digits =1)
-
-attrition_by_treatment_table
-
-########################################
-#attritors vs compliants for girls only#
-########################################
-#omits girl[2], test_observed[13] and test_score[19]
-attrition_for_girls <- lapply(varlist[c(1,3:12,15:18,20:23)], function(x) {
-  t.test(as.formula(paste(x,"test_observed",sep="~")), data = subset(afghan,girl==1)
-         , alternative = "two.sided", mu = 0, paired = FALSE, var.equal = FALSE, conf.level = 0.95)
-})
-
-#create table
-girl_attrition_table <- t(sapply(attrition_for_girls, function(x) {
-  c(x$data.name,
-    x$estimate,
-    ci.lower = x$conf.int[1],
-    ci.upper = x$conf.int[2],
-    p.value = x$p.value,
-    "adj.p.value" = p.adjust(x$p.value, method = "bonferroni", n = length(x)),
-    x$parameter
-  )
-}))
-rownames(girl_attrition_table) <- girl_attrition_table[,1]
-girl_attrition_table <- girl_attrition_table[,-c(1)]
-girl_attrition_table <- data.frame(girl_attrition_table, stringsAsFactors=FALSE)
-girl_attrition_table[] <- lapply(girl_attrition_table, function(x) as.numeric(as.character(x)))
-girl_attrition_table<-format(round(girl_attrition_table,3),digits =1)
-girl_attrition_table
 
 ##############################################################################
 #Q3 -  Baseline Enrollment                                                   #
